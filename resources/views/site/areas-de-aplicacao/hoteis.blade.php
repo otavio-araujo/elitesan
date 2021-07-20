984ec4bcb113c396aa0470481a77a301d38278ab
Otávio Araújo <30505243+otavio-araujo@users.noreply.github.com>
2021-07-20 16:41:20 -0300

    Update hoteis.blade.php

--- resources/views/site/areas-de-aplicacao/hoteis.blade.php
+++ resources/views/site/areas-de-aplicacao/hoteis.blade.php
@@ -98,7 +98,6 @@
                 <li><i class="fa fa-check text-info"></i> Redução do risco de contaminação cruzada entre hóspedes;</li>
                 <li><i class="fa fa-check text-info"></i> Diferencial competitivo para o hotel que coloca a saúde dos hóspedes como uma preocupação essencial;</li>
                 <li><i class="fa fa-check text-info"></i> Ambiente limpo e livre de micro-organismos, como fungos e bactérias;</li>
-                <li><i class="fa fa-check text-info"></i> Película protetora que previne a proliferação futura de material biológico infectante;</li>
                 <li><i class="fa fa-check text-info"></i> Possibilidade de agendamento da sanitização de indústria de acordo com os horários disponíveis do cliente;</li>
                 <li><i class="fa fa-check text-info"></i> Não deixa resíduos, não mancha e não deixa cheiro;</li>
                 <li><i class="fa fa-check text-info"></i> Aplicação relativamente rápida;</li>
