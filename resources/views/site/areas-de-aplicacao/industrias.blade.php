598f8e00b5b633a29e98f358d6861a93b69e2daa
Otávio Araújo <30505243+otavio-araujo@users.noreply.github.com>
2021-07-20 16:41:36 -0300

    Update industrias.blade.php

--- resources/views/site/areas-de-aplicacao/industrias.blade.php
+++ resources/views/site/areas-de-aplicacao/industrias.blade.php
@@ -90,7 +90,6 @@
                 <li><i class="fa fa-check text-info"></i> Produto inovador e ecologicamente correto, assegurando a qualidade de vida, preservando a saúde e o meio ambiente;</li>
                 <li><i class="fa fa-check text-info"></i> Equipamentos de tecnologia de ponta para correta aspersão do produto;</li>
                 <li><i class="fa fa-check text-info"></i> Ambiente limpo e livre de micro-organismos, como fungos e bactérias;</li>
-                <li><i class="fa fa-check text-info"></i> Película protetora que previne a proliferação futura de material biológico infectante;</li>
                 <li><i class="fa fa-check text-info"></i> Possibilidade de agendamento da sanitização de indústria de acordo com os horários disponíveis do cliente;</li>
                 <li><i class="fa fa-check text-info"></i> Não deixa resíduos, não mancha e não deixa cheiro;</li>
                 <li><i class="fa fa-check text-info"></i> Aplicação relativamente rápida;</li>
