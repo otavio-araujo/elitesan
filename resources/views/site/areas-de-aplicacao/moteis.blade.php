59f7cff456d9995013b41214eb4258a82d87705e
Otávio Araújo <30505243+otavio-araujo@users.noreply.github.com>
2021-07-20 16:42:16 -0300

    Update moteis.blade.php

--- resources/views/site/areas-de-aplicacao/moteis.blade.php
+++ resources/views/site/areas-de-aplicacao/moteis.blade.php
@@ -92,7 +92,6 @@
                 <li><i class="fa fa-check text-info"></i> Redução do risco de contaminação cruzada entre os clientes e colaboradores;</li>
                 <li><i class="fa fa-check text-info"></i> Diferencial competitivo para o motel que coloca a saúde do cliente como uma preocupação essencial;</li>
                 <li><i class="fa fa-check text-info"></i> Equipamentos e utencílios limpos e livre de micro-organismos, como fungos e bactérias;</li>
-                <li><i class="fa fa-check text-info"></i> Película protetora que previne a proliferação futura de material biológico infectante;</li>
                 <li><i class="fa fa-check text-info"></i> Possibilidade de agendamento da sanitização de motéis de acordo com os horários disponíveis do cliente;</li>
                 <li><i class="fa fa-check text-info"></i> Não deixa resíduos, não mancha e não deixa cheiro;</li>
                 <li><i class="fa fa-check text-info"></i> Aplicação relativamente rápida;</li>
